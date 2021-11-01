71b0c6158f5f7ba21de64dd9096122a12350abb0
hallissonkite <charlicq@hotmail.com>
2021-11-01 19:26:50 -0300

    Projeto criados

---
+++ backend/src/main/java/com/devsuperior/devendas/DevendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/devendas/DevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

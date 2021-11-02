a92af996ea08ac10e13fabf96cd90bb17a0e6a24
hallissonkite <charlicq@hotmail.com>
2021-11-02 00:38:45 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/hallissonkite" target="_blank" rel="noreferrer">Hallisson kite</a></p>
+          <p className="text-light"><small><strong>Hallisson Kite Enterprises</strong><br/>
+            Esta obra prima foi desenvolvido por: <a href="https://instagram.com/hallissonkite" target="_blank" rel="noreferrer">@hallissonkite</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;

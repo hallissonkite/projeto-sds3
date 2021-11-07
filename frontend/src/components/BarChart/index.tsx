150d0c1dd4a1034bf4c24851c0330e75173f86d7
hallissonkite <charlicq@hotmail.com>
2021-11-06 23:13:45 -0300

    DataTable integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios from "axios";
 import Chart from 'react-apexcharts';
 import { SaleSucess } from 'types/sale';
 import { round } from 'Utils/format';
--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,88 +1,52 @@
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import axios from "axios";
+import { BASE_URL } from "Utils/Requests";
+import { setSourceMapRange } from "typescript";
+import { formatLocalDate } from "Utils/format";
+import { truncate } from "fs";
 
 const DataTable = () => {
+
+    const[ page, setPage]= useState<SalePage>({
+        first:true,
+        number:0,
+        totalElements:0,
+        totalPages:0,
+        last:true
+    });
+
+    useEffect(() =>{
+
+    axios.get(`${BASE_URL}/sales?page=4`).then(response=>{
+        setPage(response.data);
+    })
+
+    },[]);
+
     return (
         <div className="table-responsive">
     <table className="table table-striped table-sm">
         <thead>
             <tr>
                 <th>Data</th>
-                <th>Instagran</th>
-                <th>Nota</th>
-                <th>Quantas x ficaram</th>
+                <th>Vendendor</th>
+                <th>Clientes Visitados</th>
+                <th>Negócios Fechados</th>
                 <th>Valor</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Luana Piovani</td>
-                <td>8</td>
-                <td>5</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Mia Khalifa</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Juliana Paes</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Fernanda Lima</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Carol dias</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Anitta</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Brunna Marquezine</td>
-                <td>10</td>
-                <td>2</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Carla Diaz</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Giulia</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
-            <tr>
-                <td>22/04/2021</td>
-                <td>Aline Riscado</td>
-                <td>34</td>
-                <td>25</td>
-                <td>15017.00</td>
-            </tr>
+            {page.content?.map(item=> (
+                 <tr key={item.id}>
+                 <td>{formatLocalDate(item.date,"dd/MM/yyyy")}</td>
+                 <td>{item.seller.name}</td>
+                 <td>{item.visited}</td>
+                 <td>{item.deals}</td>
+                 <td>{item.amount.toFixed(2)}</td>
+             </tr>
+            ))}
+            
         </tbody>
     </table>
 </div>
